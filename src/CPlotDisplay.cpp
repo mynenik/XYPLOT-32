mynenik/XYPLOT-32

commit 481ce6c434c685426d3f1ea7e8cc1298f8bb6c21

Added CPlotView class and revised the CPlotDisplay class; added Forth word for retrieving grid info.
 Changes to be committed:
	modified:   modules/grace.4th
	modified:   modules/xyplot.4th
	modified:   src/CGrid.cpp
	modified:   src/CGrid.h
	modified:   src/CPlotDisplay.cpp
	modified:   src/CPlotDisplay.h
	new file:   src/CPlotView.cpp
	new file:   src/CPlotView.h
	modified:   src/CPlotWindow.cpp
	modified:   src/Makefile
	modified:   src/xyplot.cpp
	modified:   src/xyplot.h

src/CPlotDisplay.cpp
@@ -1,10 +1,10 @@
 // CPlotDisplay.cpp
 //
-// Copyright 1996--2018 Krishna Myneni 
+// Copyright 1996--2020 Krishna Myneni 
 // <krishna.myneni@ccreweb.org>
 //
 // This software is provided under the terms of the
-// GNU General Public License (GPL), v3.0 or later.
+// GNU Affero General Public License (AGPL), v 3.0 or later.
 //
 
 #include "CPlotDisplay.h"
@@ -17,25 +17,20 @@ CPlotDisplay::CPlotDisplay()
 {
     m_fAspect = 1.;
     m_pPlotList = new CPlotList();
-	vector<float> x(4);
-	x[0] = -1.;
-	x[1] = 1.;
-	x[2] = -1.;
-	x[3] = 1.;
+    vector<float> x(4);
+    x[0] = -1.;
+    x[1] = 1.;
+    x[2] = -1.;
+    x[3] = 1.;
 
-    m_pGrid = NULL;
     CreateView (CARTESIAN, x);
 }
 //---------------------------------------------------------------
 
 CPlotDisplay::~CPlotDisplay()
 {
-    delete m_pGrid;
-
-    m_qCT.erase(m_qCT.begin(), m_qCT.end());
-
+    m_qPV.erase(m_qPV.begin(), m_qPV.end());
     delete m_pPlotList;
-
 }
 //---------------------------------------------------------------
 
@@ -43,7 +38,7 @@ void CPlotDisplay::SetExtrema(vector<float> x)
 {
 // Set extrema for the current view
 
-    (**m_pCt).SetLogical(x);
+    (*m_qiView)->SetExtrema(x);
     strcpy (m_szXform, DisplayFormat(x[0],x[1]));
     strcpy (m_szYform, DisplayFormat(x[2],x[3]));
 }
@@ -51,37 +46,9 @@ void CPlotDisplay::SetExtrema(vector<float> x)
 
 void CPlotDisplay::CreateView(COORDINATE_SYSTEM cdns, vector<float> x)
 {
-
-  CTransform* pCt;
-
-  switch (cdns)
-    {
-    case CARTESIAN:
-      pCt = new C2D_Transform();
-      m_qCT.push_back(pCt);
-      delete m_pGrid;
-      m_pGrid = new CCartesianGrid();
-      break;
-    case CARTESIAN_INVERTED:
-      pCt = new C2Di_Transform();
-      m_qCT.push_back(pCt);
-      delete m_pGrid;
-      m_pGrid = new CCartesianGrid();
-      break;
-    case POLAR:
-      pCt = new C2D_PolarTransform();
-      m_qCT.push_back(pCt);
-      delete m_pGrid;
-      m_pGrid = new CPolarGrid();
-      break;
-    default:
-      ;
-    }
-
-  m_pGrid->SetTransform (pCt);
-  m_pCt = m_qCT.end() - 1;
-  SetExtrema (x);
-
+    CPlotView* pv = new CPlotView(cdns, x);
+    m_qPV.push_back(pv);
+    m_qiView = m_qPV.end() - 1;
 }
 //---------------------------------------------------------------
 /*
@@ -127,8 +94,7 @@ void CPlotDisplay::SetPlotRect (CRect wRect, CDC* pDC)
 	    }
         }
 
-    (**m_pCt).SetPhysical (wRect);
-    m_pGrid->SetTransform (*m_pCt);
+    (*m_qiView)->m_pCt->SetPhysical (wRect);
 }
 //---------------------------------------------------------------
 
@@ -136,9 +102,8 @@ float CPlotDisplay::GetAspect()
 {
 // Return aspect ratio of current display
 
-    CRect wRect = (**m_pCt).GetPhysical();
+    CRect wRect = (*m_qiView)->m_pCt->GetPhysical();
     return (((float) wRect.Width())/wRect.Height());
-
 }
 //---------------------------------------------------------------
 
@@ -166,9 +131,7 @@ CPlot* CPlotDisplay::MakePlot (CWorkspace41* ws, CDataset* ds, int n)
 {
 // Create corresponding plot for plot n in a 4.1 workspace
 
-    CPlot* p = NULL;
-
-    p = new CXyPlot (ds);
+    CPlot* p = new CXyPlot (ds);
     switch (ws->pv[n].sym)
     {
         case 'p':
@@ -187,10 +150,7 @@ CPlot* CPlotDisplay::MakePlot (CWorkspace41* ws, CDataset* ds, int n)
           ;
     }
 
-    // Add to plot list
-
     m_pPlotList->AddPlot (p);
-
     return p;
 }
 //---------------------------------------------------------------
@@ -220,7 +180,6 @@ void CPlotDisplay::DeletePlot ()
 // Delete the active plot in the plot list
 
     CPlot* p = m_pPlotList->Active();
-
     m_pPlotList->RemovePlot(p);
     delete p;
 }
@@ -235,7 +194,6 @@ void CPlotDisplay::DeletePlotsOf (CDataset* d)
     while (p = m_pPlotList->PlotOf(d))
     {
         m_pPlotList->RemovePlot(p);
-
         delete p;
     }
 }
@@ -243,12 +201,12 @@ void CPlotDisplay::DeletePlotsOf (CDataset* d)
 
 void CPlotDisplay::Draw(CDC *pDC)
 {
+  CPlotView* pView = *m_qiView;
+  pView->m_pGrid->SetColor(pDC->GetColor("black"));  // set color of grid
+  pView->m_pGrid->Draw(pDC);
+  pView->m_pGrid->Labels(pDC);
 
-  m_pGrid->SetColor(pDC->GetColor("black"));  // set color of grid
-  m_pGrid->Draw(pDC);
-  m_pGrid->Labels(pDC);
-
-  CRect rect = (**m_pCt).GetPhysical();
+  CRect rect = pView->m_pCt->GetPhysical();
   pDC->SetClipRect(rect);  // enable clipping for plot area
   m_pPlotList->Draw(pDC);
   pDC->SetClipRect (pDC->GetClientRect()); // disable clipping
@@ -261,33 +219,41 @@ void CPlotDisplay::ResetExtrema()
 //   which there are plots
 
   vector<float> x = m_pPlotList->GetExtrema();
-  (**m_qCT.begin()).SetLogical(x);
+  (*m_qPV.begin())->SetExtrema(x);
   strcpy (m_szXform, DisplayFormat(x[0],x[1]));
   strcpy (m_szYform, DisplayFormat(x[2],x[3]));
 }
 //---------------------------------------------------------------
 
+void CPlotDisplay::ApplyCurrentView()
+{
+   CTransform* pT = (*m_qiView)->m_pCt;
+   if (pT) (*m_qiView)->m_pGrid->SetTransform(pT);
+}
+
 void CPlotDisplay::GoBack()
 {
-// Back up to the previous coordinate transformation in
-//   a circular fashion
+// Back up to the previous view in a circular fashion
 
-    if (m_pCt > m_qCT.begin())
-        --m_pCt;
+    if (m_qiView > m_qPV.begin())
+        --m_qiView;
     else
-        m_pCt = m_qCT.end() - 1;
+        m_qiView = m_qPV.end() - 1;
+
+    ApplyCurrentView();
 }
 //---------------------------------------------------------------
 
 void CPlotDisplay::GoForward()
 {
-// Move to the next coordinate transformation in the queue,
-//   in a circular fashion
+// Move to the next view in the queue, in a circular fashion
 
-    if (m_pCt < (m_qCT.end() - 1))
-        ++m_pCt;
+    if (m_qiView < (m_qPV.end() - 1))
+        ++m_qiView;
     else
-        m_pCt = m_qCT.begin();
+        m_qiView = m_qPV.begin();
+
+    ApplyCurrentView();
 }
 //---------------------------------------------------------------
 
@@ -296,10 +262,11 @@ void CPlotDisplay::DeleteView ()
 // Delete the current transformation and go to previous one,
 //   unless it is the first transformation.
 
-    if (m_pCt > m_qCT.begin())
+    if (m_qiView > m_qPV.begin())
     {
-        --m_pCt;
-        m_qCT.erase (m_pCt + 1, m_pCt + 2);
+        --m_qiView;
+        m_qPV.erase (m_qiView + 1, m_qiView + 2);
+	ApplyCurrentView();
     }
 }
 //---------------------------------------------------------------
@@ -329,6 +296,4 @@ int CPlotDisplay::Write (ofstream& ofile)
 
   return 0;
 }
-
-
 

src/CPlotDisplay.h
@@ -2,11 +2,11 @@
 //
 // class CPlotDisplay 
 //
-// Copyright 1996-2018 Krishna Myneni
+// Copyright 1996-2020 Krishna Myneni
 // <krishna.myneni@ccreweb.org>
 //
 // This software is provided under the terms of the
-// GNU General Public License (GPL), v3.0 or later.
+// GNU Affero General Public License (AGPL), v 3.0 or later.
 //
 
 #ifndef __CPLOTDISPLAY_H__
@@ -17,40 +17,36 @@
 #include <fstream>
 #include "CPlotList.h"
 #include "CPolarGrid.h"
+#include "CPlotView.h"
 #include "CXyPlot.h"
 #include "CWorkspace41.h"
 
-using std::vector;
-using std::deque;
-using std::ifstream;
-using std::ofstream;
-
-enum COORDINATE_SYSTEM {CARTESIAN, CARTESIAN_INVERTED, POLAR};
+using namespace std;
 
 class CPlotDisplay {
-  deque<CTransform*> m_qCT;    // double ended queue of coordinate transforms
-  deque<CTransform*>::iterator m_pCt;  // iterator to current transform
+  deque<CPlotView*> m_qPV;    // double ended queue of pointers to plot views
+  deque<CPlotView*>::iterator m_qiView;  // iterator to current plot view
   CPlotList* m_pPlotList;     // the plot list
   float m_fAspect;            // plot display desired aspect ratio
 public:
-  CGrid* m_pGrid;             // ptr to grid object
   CPoint m_nMousePt;
   char m_szXform[16];
   char m_szYform[16];
 
   CPlotDisplay ();
   ~CPlotDisplay ();
+  CPlotView* GetCurrentView() { return (*m_qiView); }
   void CreateView (COORDINATE_SYSTEM, vector<float>);
+  void ApplyCurrentView();
   void DeleteView ();
   void GoBack();
   void GoForward();
-  void GoHome() {m_pCt = m_qCT.begin();}
-// void SetViewAngles (float, float);
+  void GoHome() {m_qiView = m_qPV.begin(); ApplyCurrentView();}
   int Nplots() {return m_pPlotList->Nplots();}
   void SetAspect (float aspect) {m_fAspect = aspect;}
   float GetAspect ();
   void SetPlotRect (CRect, CDC*);
-  CRect GetPlotRect () {return (**m_pCt).GetPhysical();}
+  CRect GetPlotRect () {return (*m_qiView)->m_pCt->GetPhysical();}
   CPlot* SelectedPlot (CPoint p) {return m_pPlotList->Selection(p);}
   CDataset* GetActiveSet ();
   CDataset* GetOperandSet ();
@@ -58,13 +54,13 @@ public:
   CPlot* GetOperandPlot () {return m_pPlotList->Operand();}
   BOOL SetActivePlot (CPlot* p) {return m_pPlotList->SetActive(p);}
   BOOL SetOperandPlot (CPlot* p) {return m_pPlotList->SetOperand(p);}
-  char* GetList () {m_pPlotList->GetList();}
+  char* GetList () {return( m_pPlotList->GetList() );}
   void DisplayList (CDC* pDC) {m_pPlotList->DisplayList(pDC);}
   void Draw (CDC*);
-  vector<float> Logical (CPoint p) {return (**m_pCt).Logical(p);}
-  vector<float> Logical (CRect r) {return (**m_pCt).Logical(r);}
+  vector<float> Logical (CPoint p) {return (*m_qiView)->m_pCt->Logical(p);}
+  vector<float> Logical (CRect r) {return (*m_qiView)->m_pCt->Logical(r);}
   void SetExtrema (vector<float> x);
-  vector<float> GetExtrema () {return (**m_pCt).GetLogical();}
+  vector<float> GetExtrema () {return (*m_qiView)->GetExtrema();}
   void ResetExtrema();
   void Reset();
   CPlot* MakePlot(CDataset*, int) ;

src/Makefile
@@ -18,7 +18,7 @@
 #      before "make debug".
 #
 COPYRIGHT=1999--2020
-VERSION=2.4.9-2
+VERSION=2.5.0
 KFORTH_VER=1.6.4-2
 MODULE_DIR_VAR=XYPLOT_DIR
 BUILD_DATE=`date +%F`
@@ -62,7 +62,7 @@ CPPFLAGS = -c -m32
 CFLAGS = -c -m32
 
 OBJS = xyplot.o CDataset.o CDatabase.o CDeviceContext.o CGrid.o\
-  CPlotDisplay.o CPlotList.o CPlotObject.o CPlotWindow.o\
+  CPlotView.o CPlotDisplay.o CPlotList.o CPlotObject.o CPlotWindow.o\
   CPolarGrid.o CTransform.o CWorkspace41.o CXyFile.o CXyPlot.o\
   CxDC.o CpsDC.o AlgebraCompiler.o ForthCompiler.o ForthVM.o vm32.o\
   vmc.o
@@ -95,7 +95,11 @@ CDeviceContext.o: CDeviceContext.cpp CDeviceContext.h CPoint.h CRect.h CPlotMess
 CGrid.o: CGrid.cpp CGrid.h CPlotObject.h CPoint.h CRect.h CDeviceContext.h CxDC.h
 	${CPP} ${CPPFLAGS} -I${INCLUDEDIRS} ${DEBUG} CGrid.cpp
 
-CPlotDisplay.o: CPlotDisplay.cpp CPlotDisplay.h CPlotList.h CPolarGrid.h\
+CPlotView.o: CPlotView.cpp CPlotView.h CTransform.h CGrid.h
+	${CPP} ${CPPFLAGS} -I${INCLUDEDIRS} ${DEBUG} CPlotView.cpp
+
+CPlotDisplay.o: CPlotDisplay.cpp CPlotDisplay.h CPlotList.h \
+	CTransform.h CGrid.h CPolarGrid.h CPlotView.h \
 	CXyPlot.h CWorkspace41.h CPoint.h CRect.h CDeviceContext.h CxDC.h
 	${CPP} ${CPPFLAGS} -I${INCLUDEDIRS} ${DEBUG} CPlotDisplay.cpp
 

modules/grace.4th
@@ -357,20 +357,33 @@ variable nplots
     s" @g0 hidden false" >grfile
 ;
 
+variable nXtics
+variable nYtics
+variable bVlines
+variable bHlines
+variable bXaxis
+variable bYaxis
+
 : write_xyplot_axes ( -- )
+    get_grid bYaxis ! bXaxis ! bHlines ! bVlines ! nYtics ! nXtics !
     s" @    xaxis on"      >grfile
     s" @    xaxis tick on" >grfile 
     s" @    xaxis tick major " 
-    xmax f@ xmin f@ f- 10e f/ 
+    xmax f@ xmin f@ f- nXtics @ s>f ( 10e) f/ 2e f* 
     6 f>$ strcat >grfile
+    bVlines @ IF
+      s" @    xaxis tick major grid on"  >grfile
+    THEN
     s" @    xaxis ticklabel on" >grfile
     s" @    yaxis on"      >grfile
     s" @    yaxis tick on" >grfile
     s" @    yaxis tick major " 
-    ymax f@ ymin f@ f- 10e f/ 
+    ymax f@ ymin f@ f- nYtics @ s>f ( 10e) f/ 2e f* 
     6 f>$ strcat >grfile
+    bHlines @ IF
+      s" @    yaxis tick major grid on"  >grfile
+    THEN
     s" @    yaxis ticklabel on" >grfile
-
     s" @    frame type 0"  >grfile
 ;
 

modules/xyplot.4th
@@ -9,7 +9,7 @@
 \
 \ Please report bugs to <krishna.myneni@ccreweb.org>
 \
-\ Requires: xyplot version >= 2.4.6
+\ Requires: xyplot version >= 2.5.0
 \
 \
 \ XYPLOT defines Forth constants which contain pointers to
@@ -35,6 +35,7 @@
 \  set_plot_symbol ( ^symbolname -- ) set active plot symbol.
 \  set_plot_color ( ^colorname -- ) set active plot color.
 \  draw_plot ( flag -- ) draw the active plot.
+\  get_grid ( -- nXtics nYtics bVLines bHLines bXaxis bYaxis ) get grid params.
 \  set_grid_tics ( nx ny -- ) set number of tics for x and y axes
 \  set_grid_lines ( flagx flagy -- ) set grid lines on/off for x and y axes 
 \  clear_window ( -- )	Clear the plot window.
@@ -131,6 +132,9 @@ prec_DOUBLE  8 LSHIFT  data_REAL  OR  constant  REAL_DOUBLE
 
 : draw_plot ( flag -- | draw active plot, if flag = false use bkg color )
 	FN_DRAW_PLOT call ;
+
+: get_grid ( -- nXtics nYtics bVlines bHlines bXaxis bYaxis )
+        FN_GET_GRID call ;
 
 : set_grid_tics ( nx ny -- | set number of tics on x and y axes )
 	FN_SET_GRID_TICS call ;

src/CGrid.cpp
@@ -1,10 +1,10 @@
 // CGrid.cpp
 //
-// Copyright 1995--2018 Krishna Myneni
+// Copyright 1995--2020 Krishna Myneni
 // <krishna.myneni@ccreweb.org>
 //
 // This software is provided under the terms of the 
-// GNU General Public License (GPL), v3.0 or later.
+// GNU Affero General Public License (AGPL), v 3.0 or later.
 //
 
 #include "CGrid.h"
@@ -13,17 +13,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-bool CGridLines::m_bHorizontal = true;
-bool CGridLines::m_bVertical = true;
-
 extern char* LabelFormat (float, float, char);
 
-
-CGridFrame::CGridFrame (int nXt, int nYt)
-{
-    if (nXt >= 0) nXtics = nXt;
-    if (nYt >= 0) nYtics = nYt;
-}
 //---------------------------------------------------------------
 
 void CGridFrame::Draw (CDC* pDc)
@@ -103,7 +94,7 @@ void CGridLines::Draw (CDC* pDc)
   ix2 = rect.BottomRight().x;
   iy2 = rect.BottomRight().y;
 
-  if (CGridLines::m_bVertical)
+  if (m_bVertical)
     {
       if (nXtics > 0)
 	{
@@ -117,7 +108,7 @@ void CGridLines::Draw (CDC* pDc)
     }
 
 
-  if (CGridLines::m_bHorizontal)
+  if (m_bHorizontal)
     {
       if (nYtics > 0)
 	{
@@ -141,16 +132,18 @@ void CAxes::Draw (CDC* pDc)
   x = m_pT->GetLogical();
   p = m_pT->Physical(ax);
 
-  if ((x[0] < ax[0]) && (x[1] > ax[0]))
-    {
+  if (m_bYaxis) {
+    if ((x[0] < ax[0]) && (x[1] > ax[0])) {
       pDc->MoveTo(p.x, r.TopLeft().y);
       pDc->LineTo(p.x, r.BottomRight().y);
     }
-  if ((x[2] < ax[1]) && (x[3] > ax[1]))
-    {
+  }
+  if (m_bXaxis) {
+    if ((x[2] < ax[1]) && (x[3] > ax[1])) {
       pDc->MoveTo(r.TopLeft().x, p.y);
       pDc->LineTo(r.BottomRight().x, p.y);
     }
+  }
 }
 
 //---------------------------------------------------------------
@@ -162,15 +155,13 @@ CGrid::CGrid()
   m_szFontName = new char[32];
   strcpy (m_szFontName, "Courier New");
 
-  m_pFrame = NULL;
-  m_pLines = NULL;
-  m_pAxes = NULL;
-
-  m_pFrame = new CGridFrame (10, 10);
+  m_pFrame = new CGridFrame ();
   m_pFrame->SetAttributes (PS_SOLID, 1, m_nColor);
-
-  SetLines(CGridLines::m_bHorizontal, CGridLines::m_bVertical);
-  SetAxes(True);
+  m_pLines = new CGridLines();
+  m_pAxes = new CAxes();
+  SetTics(10, 10);
+  SetLines(false, false);
+  SetAxes(true, true);
 }
 //---------------------------------------------------------------
 CGrid::~CGrid()
@@ -184,16 +175,8 @@ CGrid::~CGrid()
 
 void CGrid::SetTics (int xtics, int ytics)
 {
-  delete m_pFrame;
-
-  if ((xtics > 0) && (ytics > 0))
-    {
-      m_pFrame = new CGridFrame(xtics, ytics);
-    }
-  else
-    {
-      m_pFrame = NULL;
-    }
+    nXtics = xtics;
+    nYtics = ytics;
 }
 
 void CGrid::GetTics (int* pXtics, int* pYtics)
@@ -202,36 +185,37 @@ void CGrid::GetTics (int* pXtics, int* pYtics)
     *pYtics = nYtics;
 }
 
-void CGrid::SetLines (bool bx, bool by)
+void CGrid::SetLines (bool bVer, bool bHor)
 {
-  delete m_pLines;
-  CGridLines::m_bVertical = bx;
-  CGridLines::m_bHorizontal = by;
+  if (m_pLines) {
+    m_pLines->m_bVertical = bVer;
+    m_pLines->m_bHorizontal = bHor;
+  }
+}
 
-  if (bx || by)
-    {
-      m_pLines = new CGridLines ();
-      m_pLines->SetAttributes (PS_DOT, 1, m_nColor);
-    }
-  else
-    {
-      m_pLines = NULL;
+void CGrid::GetLines (bool* pHor, bool* pVer)
+{
+    if ( m_pLines) {
+      *pVer = m_pLines->m_bVertical;
+      *pHor = m_pLines->m_bHorizontal;
     }
 }
 
-void CGrid::SetAxes (bool b)
+void CGrid::SetAxes (bool bx, bool by)
 {
-  delete m_pAxes;
-
-  if (b)
-    {
-      m_pAxes = new CAxes ();
+  if (m_pAxes) {
       m_pAxes->SetAttributes (PS_SOLID, 1, m_nColor);
-    }
-  else
-    {
-      m_pAxes = NULL;
-    }
+      m_pAxes->m_bXaxis = bx;
+      m_pAxes->m_bYaxis = by;
+  }
+}
+
+void CGrid::GetAxes (bool* pX, bool* pY)
+{
+  if (m_pAxes) {
+    *pX = m_pAxes->m_bXaxis;
+    *pY = m_pAxes->m_bYaxis;
+  }
 }
 //---------------------------------------------------------------
 
@@ -240,15 +224,12 @@ void CCartesianGrid::Draw(CDC* pDc)
   pDc->SetForeground(m_nColor);
 
   // Draw the frame and tics
-
   if (m_pFrame) m_pFrame->Draw(pDc);
 
   // Draw the grid lines
-
   if (m_pLines) m_pLines->Draw(pDc);
 
   // Draw the axes
-
   if (m_pAxes) m_pAxes->Draw(pDc);
 
 }

src/CGrid.h
@@ -2,11 +2,11 @@
 //
 // class CGrid
 //
-// Copyright 1996--2018 Krishna Myneni
+// Copyright 1996--2020 Krishna Myneni
 // <krishna.myneni@ccreweb.org>
 //
 // This software is provided under the terms of the 
-// GNU General Public License (GPL), v3.0 or later.
+// GNU Affero General Public License (GPL), v3.0 or later.
 //
 
 #ifndef __CGRID_H__
@@ -23,15 +23,14 @@ static int nYt [MAX_TICS];
 class CGridFrame : public CPlotObject
 {
 public:
-  CGridFrame (int xtics, int ytics);
   void Draw (CDC*);
 };
 
 class CGridLines : public CPlotObject
 {
 public:
-  static bool m_bHorizontal;
-  static bool m_bVertical;
+  bool m_bHorizontal;
+  bool m_bVertical;
   void Draw (CDC*);
 };
 
@@ -39,6 +38,8 @@ public:
 class CAxes : public CPlotObject
 {
 public:
+  bool m_bXaxis;
+  bool m_bYaxis;
   void Draw (CDC*);
 };
 
@@ -50,12 +51,15 @@ public:
   CAxes* m_pAxes;
   int m_nFontPointSize;
   char* m_szFontName;
-CGrid ();
+
+  CGrid ();
   ~CGrid ();
   void SetTics (int, int);
   void GetTics (int*, int*);
   void SetLines (bool, bool);
-  void SetAxes (bool);
+  void GetLines (bool*, bool*);
+  void SetAxes (bool, bool);
+  void GetAxes (bool*, bool*);
 
   virtual void Labels (CDC*) = 0;
   virtual void Draw (CDC*) = 0;

src/CPlotView.cpp
@@ -1,0 +1,59 @@
+// CPlotView.cpp
+//
+// Copyright 2020 Krishna Myneni
+//
+// This software is provided under the terms of the
+// GNU Affero General Public License (AGPL) v 3.0 or later.
+//
+
+#include <vector>
+using namespace std;
+#include "CTransform.h"
+#include "CPlotObject.h"
+#include "CGrid.h"
+#include "CPolarGrid.h"
+#include "CPlotView.h"
+
+CPlotView::CPlotView( COORDINATE_SYSTEM cdns, vector<float> x )
+{
+    m_pCt = NULL;
+    m_pGrid = NULL;
+
+    switch (cdns)
+    {
+      case CARTESIAN:
+        m_pCt   = new C2D_Transform();
+        m_pGrid = new CCartesianGrid();
+        break;
+      case CARTESIAN_INVERTED:
+        m_pCt   = new C2Di_Transform();
+        m_pGrid = new CCartesianGrid();
+        break;
+      case POLAR:
+        m_pCt   = new C2D_PolarTransform();
+        m_pGrid = new CPolarGrid();
+        break;
+       default:
+         ;
+    }
+    m_pGrid->SetTransform (m_pCt);
+    SetExtrema (x);
+}
+
+CPlotView::~CPlotView()
+{
+    delete m_pGrid;
+    delete m_pCt;
+}
+
+void CPlotView::SetExtrema(vector<float> x)
+{
+     m_pCt->SetLogical(x);
+}
+
+vector<float> CPlotView::GetExtrema()
+{
+    return( m_pCt->GetLogical() );
+}
+
+

commit 510edff436babb9d36f0b1f1969d02194becaf88

Fixed bug in cursor coordinate display precision, introduced in commit 481ce6c
 Changes to be committed:
	modified:   src/CPlotDisplay.cpp
	modified:   src/CPlotDisplay.h
	modified:   src/Makefile

src/CPlotDisplay.cpp
@@ -33,11 +33,8 @@ CPlotDisplay::~CPlotDisplay()
 }
 //---------------------------------------------------------------
 
-void CPlotDisplay::SetExtrema(vector<float> x)
+void CPlotDisplay::SetCoordinateDisplayFormat(vector<float> x)
 {
-// Set extrema for the current view
-
-    (*m_qiView)->SetExtrema(x);
     strcpy (m_szXform, DisplayFormat(x[0],x[1]));
     strcpy (m_szYform, DisplayFormat(x[2],x[3]));
 }
@@ -46,14 +43,17 @@ void CPlotDisplay::SetExtrema(vector<float> x)
 void CPlotDisplay::CreateView(COORDINATE_SYSTEM cdns, vector<float> x)
 {
     CPlotView* pv = new CPlotView(cdns, x);
-    m_qPV.push_back(pv);
-    if (m_qiView >= m_qPV.begin()) {
-      // new plot view inherits some properties from previous view
-      bool bXlines, bYlines, bXaxes, bYaxes;
-      (*m_qiView)->m_pGrid->GetLines(&bXlines, &bYlines);
-      (*m_qiView)->m_pGrid->GetAxes(&bXaxes, &bYaxes);
-      pv->m_pGrid->SetLines(bXlines, bYlines);
-      pv->m_pGrid->SetAxes(bXaxes, bYaxes);
+    if (pv) {
+      m_qPV.push_back(pv);
+      this->SetCoordinateDisplayFormat(x);
+      if (m_qiView >= m_qPV.begin()) {
+        // new plot view inherits some properties from previous view
+        bool bXlines, bYlines, bXaxes, bYaxes;
+        (*m_qiView)->m_pGrid->GetLines(&bXlines, &bYlines);
+        (*m_qiView)->m_pGrid->GetAxes(&bXaxes, &bYaxes);
+        pv->m_pGrid->SetLines(bXlines, bYlines);
+        pv->m_pGrid->SetAxes(bXaxes, bYaxes);
+      }
     }    
     m_qiView = m_qPV.end() - 1;
 }
@@ -227,15 +227,18 @@ void CPlotDisplay::ResetExtrema()
 
   vector<float> x = m_pPlotList->GetExtrema();
   (*m_qPV.begin())->SetExtrema(x);
-  strcpy (m_szXform, DisplayFormat(x[0],x[1]));
-  strcpy (m_szYform, DisplayFormat(x[2],x[3]));
+  this->SetCoordinateDisplayFormat(x);
 }
 //---------------------------------------------------------------
 
 void CPlotDisplay::ApplyCurrentView()
 {
    CTransform* pT = (*m_qiView)->m_pCt;
-   if (pT) (*m_qiView)->m_pGrid->SetTransform(pT);
+   if (pT) {
+     (*m_qiView)->m_pGrid->SetTransform(pT);
+     vector<float> x = this->GetExtrema();
+     this->SetCoordinateDisplayFormat(x);
+   }
 }
 
 void CPlotDisplay::GoBack()

src/CPlotDisplay.h
@@ -59,7 +59,8 @@ public:
   void Draw (CDC*);
   vector<float> Logical (CPoint p) {return (*m_qiView)->m_pCt->Logical(p);}
   vector<float> Logical (CRect r) {return (*m_qiView)->m_pCt->Logical(r);}
-  void SetExtrema (vector<float> x);
+  void SetCoordinateDisplayFormat (vector<float> x);
+  void SetExtrema (vector<float> x) {(*m_qiView)->SetExtrema(x);}
   vector<float> GetExtrema () {return (*m_qiView)->GetExtrema();}
   void ResetExtrema();
   void Reset();

src/Makefile
@@ -18,7 +18,7 @@
 #      before "make debug".
 #
 COPYRIGHT=1999--2020
-VERSION=2.6.0
+VERSION=2.6.1
 KFORTH_VER=1.6.6
 MODULE_DIR_VAR=XYPLOT_DIR
 BUILD_DATE=`date +%F`